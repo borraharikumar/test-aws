8e5d891de5eedf4419ac8fcdb16ae72fe68936ff
Harikumar Borra <Harikumar Borra@DESKTOP-0QITJQM.mshome.net>
2025-01-10 14:52:13 +0545

    First commit

---
+++ src/test/java/com/mvc/SpringBootMvc01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvc01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
